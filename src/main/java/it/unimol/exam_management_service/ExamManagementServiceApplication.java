bfcf90eec8f935942276aa50e09d6bcccc81550f
Luca <Laneseluca@gmail.com>
2025-05-20 09:56:10 +0200

    Aggiunto boilerplate springboot

---
+++ src/main/java/it/unimol/exam_management_service/ExamManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package it.unimol.exam_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unimol/exam_management_service/ExamManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unimol.exam_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
